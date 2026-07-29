bac07d436ad9b5616ccfcb6132e78d66ffcee34f
heis448 <kevohero004@gmail.com>
2026-07-29 11:35:41 +0300

    Update useSocket.js

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react'
 import { io } from 'socket.io-client'
 import { useGameStore } from '../store/gameStore'
 
-const SOCKET_URL = ''
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000'
 
 let socket = null
 
